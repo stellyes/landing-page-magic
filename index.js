7dd98499407a68fdf235a5ab19e2c8963ae50fc7
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-19 22:34:24 -0700

    Add html file

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-inquirer.prompt([
-  {
-    message: "Please enter your full name: ",
+inquirer
+  .prompt([
+    {
+    type: "input",
+    message: "Enter your name",
     name: "name",
-  },
-  {
-    message: "",
-  },
-]);
+    },
+    {
+      type: "input",
+      message: "Enter your location",
+      name: "location",
+    },
+    {
+      type: "input",
+      message: "Enter your bio",
+      name: "bio",
+    },
+        {
+      type: "input",
+      message: "Enter your GitHub username",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "Enter your LinkedIn URL",
+      name: "linkedIn",
+    },
+  ])
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios
+    .get(queryUrl)
+    .then(function(data) {
+      console.log(data);
+      // console.log(data.name);
+      
+      const nameArray = [];
+      const nameString = "";
+      let numRepos = 0;
+      for (const repo in data) {
+        const {name} = data.name;
+
+        nameArray.push(name);
+
+        nameString += `${name}\n`
+        numRepos++;
+      }
+
+      console.log(nameArray);
+      console.log(nameString);
+
+      fs.writeFile("namesFile.txt", nameString, (err) => {
+        (err) ? console.error(err) : console.log(`File created successfully!\n${numRepos} saved!`);
+      });
+    });
+  });
+
+
+
